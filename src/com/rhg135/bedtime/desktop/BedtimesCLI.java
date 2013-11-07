6c128a43046a40e551ffbf4f112e9e728fc188d2
rhg135 <ricardo.gomez331@gmail.com>
2013-11-07 17:05:27 -0600

    Formatting fixes

--- src/com/rhg135/bedtime/desktop/BedtimesCLI.java
+++ src/com/rhg135/bedtime/desktop/BedtimesCLI.java
@@ -6,22 +6,23 @@ import com.rhg135.bedtime.utils.Bedtimes;
 import com.rhg135.bedtime.utils.TakeIterator;
 
 public class BedtimesCLI {
-public static void main(String[] args) {
-	if (args.length != 1) {
-		System.err.println("USAGE: java BedtimesCLI <startHour>)");
-		System.exit(2);
-	}
 	
-	final Number initialHour = Double.parseDouble(args[0]);
-	Iterable<Number> it = new Iterable<Number>() {
-		public Iterator<Number> iterator() {
-			return new TakeIterator<Number>(6, Bedtimes.bedtimeIterator(initialHour));
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("USAGE: java BedtimesCLI <startHour>)");
+			System.exit(2);
 		}
-	};
-	for (Number t: it) {
-		System.out.println("Time: " + t);
+	
+		final Number initialHour = Double.parseDouble(args[0]);
+		Iterable<Number> it = new Iterable<Number>() {
+			public Iterator<Number> iterator() {
+				return new TakeIterator<Number>(6, Bedtimes.bedtimeIterator(initialHour));
+			}
+		};
+		for (Number t: it) {
+			System.out.println("Time: " + t);
+		}
+		System.exit(0);
 	}
-	System.exit(0);
-}
 
 }
